a67c427f08a3a32c2f0c6e00240348b40a9fbb3e
Priyanka <priyanka980611@gmail.com>
2020-09-29 12:51:33 -0700

    Updates

---
+++ precache-manifest.b9f73e861b2b972ceb93da75cf4f3d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac53c3372d3c5f83ad74675d2d020675",
+    "url": "/story/index.html"
+  },
+  {
+    "revision": "3783bb85da929668740a",
+    "url": "/story/static/css/main.bd859e22.chunk.css"
+  },
+  {
+    "revision": "5d49436de619d0eadb90",
+    "url": "/story/static/js/2.1df16a8a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/story/static/js/2.1df16a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3783bb85da929668740a",
+    "url": "/story/static/js/main.2d062bae.chunk.js"
+  },
+  {
+    "revision": "2e441c5f3074ae955afa",
+    "url": "/story/static/js/runtime-main.7ce7b819.js"
+  }
+]);
